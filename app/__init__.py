e7d3097a69f73405b746ce17fdab911db232780d
Bryan Lim <ytbryan@users.noreply.github.com>
2022-03-26 13:09:12 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     login_manager = LoginManager()
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = '/login'
     return app, db, login_manager
 
 app, db, login_manager = create_app()
